6c377107c15db33f589dbc60d43073ee516ec1ee
itsmeng <nkuanmeng@gmail.com>
2024-09-12 10:38:53 +0800

    str doc

--- src/email_lib/send_email.py
+++ src/email_lib/send_email.py
@@ -1,6 +1,11 @@
 import logging
 
 def send_email(email_data):
+    """Send an email with the provided data.
+
+    Args:
+        email_data (dict): A dictionary containing 'receiver', 'subject', and 'body' of the email.
+    """
     logging.info(f"Sending email to {email_data['receiver']} with subject {email_data['subject']}")
     logging.info(f"Email body: {email_data['body']}")
     pass
